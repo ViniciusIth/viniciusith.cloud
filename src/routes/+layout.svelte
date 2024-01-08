e9f37b67a0cf34cd9432b9ec014b98fcc369c0b0
ViniciusIth <vini.ithalo@hotmail.com>
2024-01-07 23:00:31 -0300

    changed sidebar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,66 +1,28 @@
 <script lang="ts">
     import '../app.postcss';
     import {AppShell} from '@skeletonlabs/skeleton';
-    import { TabGroup, TabAnchor } from '@skeletonlabs/skeleton';
     import { page } from '$app/stores';
+    import { LightSwitch } from '@skeletonlabs/skeleton';
 
     const mainPages: Array<string> = [
-      "about",
-      "experience",
-      "education",
-      "skills",
+      "resume",
       "projects",
+      "blog",
     ]
-
-    // Highlight JS
-    // import hljs from 'highlight.js/lib/core';
-    // import 'highlight.js/styles/github-dark.css';
-    // import {storeHighlightJs} from '@skeletonlabs/skeleton';
-    // import xml from 'highlight.js/lib/languages/xml'; // for HTML
-    // import css from 'highlight.js/lib/languages/css';
-    // import javascript from 'highlight.js/lib/languages/javascript';
-    // import typescript from 'highlight.js/lib/languages/typescript';
-    //
-    // hljs.registerLanguage('xml', xml); // for HTML
-    // hljs.registerLanguage('css', css);
-    // hljs.registerLanguage('javascript', javascript);
-    // hljs.registerLanguage('typescript', typescript);
-    // storeHighlightJs.set(hljs);
 </script>
 
 <AppShell>
   <svelte:fragment slot="sidebarLeft">
-    <div class="bg-surface-100-800-token hidden h-full items-center justify-center px-28 md:grid ">
-      <div class="grid justify-center gap-4">
-        {#each mainPages as pageName}
-          <div>
+    <div class="hidden h-full items-center px-44 grid-rows-[1fr_auto] border-r border-surface-500-400-token md:grid">
+      <div class="items-center md:grid">
+        <div class="grid gap-4">
+          {#each mainPages as pageName}
             <a href="{pageName}" class="text-xl" class:bg-primary-active-token={$page.url.pathname === `/${pageName}`}>{pageName}</a>
-          </div>
-        {/each}
+          {/each}
+        </div>
       </div>
+      <LightSwitch class="justify-self-center mb-5"/>
     </div>
   </svelte:fragment>
-    
-  <svelte:fragment slot="header">
-    <TabGroup 
-      justify="justify-center"
-      active="variant-filled-primary"
-      hover="hover:variant-soft-primary"
-      flex="flex-1 lg:flex-none"
-      rounded=""
-      border=""
-      class="bg-surface-100-800-token w-full md:hidden"
-    >
-      {#each mainPages as pageName}
-        <TabAnchor href={pageName} selected={$page.url.pathname === `/${pageName}`}>
-          <svelte:fragment slot="lead">(icon)</svelte:fragment>
-          <span>{pageName}</span>
-        </TabAnchor>
-      {/each}
-    </TabGroup>
-  </svelte:fragment>
-
-  <main class="px-28">
-    <slot />
-  </main>
+  <slot/>
 </AppShell>
