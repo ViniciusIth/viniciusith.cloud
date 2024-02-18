d30d314f0f27cff91c01f9f136282eed8c9f7b24
ViniciusIth <vini.ithalo@hotmail.com>
2024-02-18 17:25:55 -0300

    fix: not found page title and go back button

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -3,12 +3,17 @@
     import tentacles from "$lib/octopus_tentacles.png";
 </script>
 
+<svelte:head>
+    <title>ViniciusIth - Not Found</title>
+</svelte:head>
+
 <div class="h-full max-h-full overflow-hidden text-center">
     <h1 class="h2 mb-5">Here be dragons</h1>
 
     <h2 class="h4">{$page.status} {$page.error?.message}</h2>
     <h2 class="h4">
         This page doesn't exist or isn't publicaly available yet.
+        <a href="/" class="anchor">Go back to main page?</a>
     </h2>
 </div>
 <img
